4ac494d21a354a53a8fb7b9b44bde8ded6ce68ca
1337deniz <dnz.yasar@hotmail.com>
2023-02-10 00:34:51 +0300

    1.0

---
+++ js/words.js
@@ -0,0 +1,63 @@
+let wordList = [
+    {
+        word: "talha",
+        ipucu: "herkes tarafından sevilen çocuk"
+    },
+    {
+        word: "deniz",
+        ipucu: "çoğunlukla asosyal"
+    },
+    {
+        word: "eyup",
+        ipucu: "kıral"
+    },
+    {
+        word: "tolga",
+        ipucu: "PAYNEPIL ÇANKIL"
+    },
+    {
+        word: "kerim",
+        ipucu: "abi 5000 karbonhidrat ye bişi olmaz"
+    },
+    {
+        word: "ahmet",
+        ipucu: "kardeşim ellerini yıkadın mı"
+    },
+    {
+        word: "eren",
+        ipucu: "sadece 2 bardak votkayla hangover oluyor"
+    },
+    {
+        word: "mert",
+        ipucu: "kırdişimnıpıysınızsizbırdıyı"
+    },
+    {
+        word: "fatos",
+        ipucu: "helü"
+    },
+    {
+        word: "emir",
+        ipucu: "api abi amınakoyim api"
+    },
+    {
+        word: "metinhoca",
+        ipucu: "evlat siz napıyosunuz orda ihğhiğ zamanında biz çok kurcaladık"
+    },
+    {
+        word: "ercan",
+        ipucu: "kardaş"
+    },
+    {
+        word: "ugur",
+        ipucu: "bir doğru var bir yanlış var"
+    },
+    {
+        word: "ali",
+        ipucu: "25 yıldır 31"
+    },
+    {
+        word: "alperen",
+        ipucu: "eeellibeeşş"
+    },
+  
+]
